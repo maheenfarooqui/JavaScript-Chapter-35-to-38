ee951bc80b465a511c89ea99e99f2e6cf3f67aad
maheen <maheenzuhra@gmail.com>
2026-02-02 19:13:38 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+// Q1. Write a function that displays current date & time in your browser.
+
+function todaytime(){
+    var time = new Date();
+    console.log(time);
+}
+todaytime();
+
+// Q2. Write a function that takes first & last name and then it 
+// greets the user using his full name.
+
+// function greetUser(){
+//     document.write(`welcome to the website ${firstName} ${lastName}`);
+    
+// }
+// var firstName =prompt("enter your first name");
+// var lastName =prompt("enter your last name");
+// greetUser();
+
+// Q3. Write a function that adds two numbers (input by user)
+// and returns the sum of two numbers.
+
+// function sum(){
+//     console.log(a+b);
+    
+// }
+// var a =Number(prompt("enter number"));
+// var b =Number(prompt("enter number"));
+
+// sum();
+
+// Q4. Calculator:
+// Write a function that takes three arguments num1, num2
+// & operator & compute the desired operation. Return and
+// show the desired result in your browser.
+function calculator(num1,num2,oper){
+    
+
+}
+calculator(num1,num2,oper)
+
+// Q5. Write a function that squares its argument.
+
+// Q6. Write a function that computes factorial of a number.
+
+// Q7. Write a function that take start and end number as inputs
+// & display counting in your browser.
+
+// Q8. Write a nested function that computes hypotenuse of a
+// right angle triangle.
+// Hypotenuse2 = Base2 + Perpendicular2
+// Take base and perpendicular as inputs.
+// Outer function : calculateHypotenuse()
+// Inner function: calculateSquare()
